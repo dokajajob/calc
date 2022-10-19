a602f705b8cc6330788e4b0ccbd19dfa9066f10e
dokajajob <dokajajob@gmail.com>
2022-10-19 11:06:14 +0300

    test addition modify text again again

--- test/test_multiply.py
+++ test/test_multiply.py
@@ -4,6 +4,7 @@ import pytest
 def test_multiply():
     result = multiply(3, 4)
     print(result)
+    print("***&")
     assert result == 12
 def test_multiply_string():
     with pytest.raises(TypeError):
