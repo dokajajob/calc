76225b5383bbd02cfe65a8bc9cf5f532b4844c32
dokajajob <dokajajob@gmail.com>
2022-10-19 11:20:50 +0300

    test addition modify text again

--- test/test_multiply.py
+++ test/test_multiply.py
@@ -1,17 +1,10 @@
-#multiply
-#test
-#remote
-#tag
 from src.calculator import multiply
 import pytest
 def test_multiply():
     result = multiply(3, 4)
-    print(result)
-    print("***&")
+
     assert result == 12
 def test_multiply_string():
     with pytest.raises(TypeError):
         multiply("string", 4)
 
-#dd
-#dd
