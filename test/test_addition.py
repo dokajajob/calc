8906f190853ff7784a5a0cdffaadf939e4dc8a9d
dokajajob <dokajajob@gmail.com>
2022-10-19 10:17:09 +0300

    test addition modify text again again

--- test/test_addition.py
+++ test/test_addition.py
@@ -2,7 +2,7 @@ from src.calculator import add
 import pytest
 def test_add():
     result = add(3, 4)
-    print(f'result of result : {result}')
+    print(f'result : {result}')
     assert result == 7
 def test_add_string():
     with pytest.raises(TypeError):
